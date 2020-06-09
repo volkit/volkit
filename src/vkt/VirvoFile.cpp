bdf729a734904b39af6bbbe5295a1547ea0033a1
Stefan Zellmann <info@szellmann.de>
2020-06-09 02:00:28 +0200

    Compile fixes

--- src/vkt/VirvoFile.cpp
+++ src/vkt/VirvoFile.cpp
@@ -1,3 +1,8 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#include <cstddef>
+#include <cstring>
 
 #include <vkt/config.h>
 
@@ -24,13 +29,22 @@ namespace vkt
 
         bool loaded() const
         {
+#if VKT_HAVE_VIRVO
             return vd.vox[0] * vd.vox[1] * vd.vox[2] > 0;
+#else
+            return false;
+#endif
         }
 
         bool load()
         {
+#if VKT_HAVE_VIRVO
             vvFileIO fileio;
             return fileio.loadVolumeData(&vd) == vvFileIO::OK;
+
+#else
+            return false;
+#endif
         }
 
         std::size_t readOffset = 0;
@@ -47,6 +61,7 @@ namespace vkt
 
     std::size_t VirvoFile::read(char* buf, std::size_t len)
     {
+#if VKT_HAVE_VIRVO
         if (!impl_->loaded() && !impl_->load())
             return 0;
 
@@ -58,15 +73,23 @@ namespace vkt
         impl_->readOffset += len;
 
         return len;
+#else
+        return 0;
+#endif
     }
 
     bool VirvoFile::good() const
     {
+#if VKT_HAVE_VIRVO
         return true; // TODO (check if exists / can be created??)
+#else
+        return false;
+#endif
     }
 
     Vec3i VirvoFile::getDims()
     {
+#if VKT_HAVE_VIRVO
         if (!impl_->loaded() && !impl_->load())
             return { 0, 0, 0 };
 
@@ -75,14 +98,21 @@ namespace vkt
             (int)impl_->vd.vox[1],
             (int)impl_->vd.vox[2]
             };
+#else
+        return {};
+#endif
     }
 
     uint16_t VirvoFile::getBytesPerVoxel()
     {
+#if VKT_HAVE_VIRVO
         if (!impl_->loaded() && !impl_->load())
             return 0;
 
         return (uint16_t)impl_->vd.bpc;
+#else
+        return 0;
+#endif
     }
 
 } // vkt
