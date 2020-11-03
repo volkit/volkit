82c73c4b63f63f3c1526d9bd35949999c5fe6c7b
Stefan Zellmann <info@szellmann.de>
2020-11-03 12:58:51 +0100

    Store dist and voxelMapping in volume file header

--- include/cpp/vkt/VolumeFile.hpp
+++ include/cpp/vkt/VolumeFile.hpp
@@ -16,6 +16,8 @@ namespace vkt
         bool isStructured = false;
         Vec3i dims = { 0, 0, 0 };
         uint16_t bytesPerVoxel = 0;
+        Vec3f dist = { 1.f, 1.f, 1.f };
+        Vec2f voxelMapping = { 0.f, 1.f };
     };
 
     class VolumeFile : public DataSource
