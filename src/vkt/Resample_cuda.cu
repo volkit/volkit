a35cdf0ed39b504b4a98863fe66b2ded6e1edc4d
Stefan Zellmann <info@szellmann.de>
2021-02-16 12:27:23 +0100

    Missing file

---
+++ src/vkt/Resample_cuda.cu
@@ -0,0 +1,16 @@
+// This file is distributed under the MIT license.
+// See the LICENSE file for details.
+
+#include "Resample_cuda.hpp"
+
+namespace vkt
+{
+
+    void Resample_cuda(
+            StructuredVolume& dst,
+            StructuredVolume& src,
+            Filter filter
+            )
+    {
+    }
+} // vkt
