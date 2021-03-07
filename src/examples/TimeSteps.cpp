092b4058fe9cd4d4744526369693dd89983ca490
Stefan Zellmann <info@szellmann.de>
2021-03-07 15:57:49 +0100

    Missing file

---
+++ src/examples/TimeSteps.cpp
@@ -0,0 +1,72 @@
+#include <cstdlib>
+#include <iostream>
+#include <ostream>
+
+#include <vkt/Array1D.hpp>
+#include <vkt/ExecutionPolicy.hpp>
+#include <vkt/InputStream.hpp>
+#include <vkt/LookupTable.hpp>
+#include <vkt/Render.hpp>
+#include <vkt/StructuredVolume.hpp>
+#include <vkt/VolumeFile.hpp>
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " file.raw\n";
+        return EXIT_FAILURE;
+    }
+
+    vkt::Array1D<vkt::StructuredVolume> volumes(argc - 1);
+
+    for (int i = 1; i < argc; ++i)
+    {
+        vkt::VolumeFile file(argv[i], vkt::OpenMode::Read);
+
+        vkt::VolumeFileHeader hdr = file.getHeader();
+
+        if (!hdr.isStructured)
+        {
+            std::cerr << "No valid volume file\n";
+            return EXIT_FAILURE;
+        }
+
+        vkt::Vec3i dims = hdr.dims;
+        if (dims.x * dims.y * dims.z < 1)
+        {
+            std::cerr << "Cannot parse dimensions from file name\n";
+            return EXIT_FAILURE;
+        }
+
+        vkt::DataFormat dataFormat = hdr.dataFormat;
+        if (dataFormat == vkt::DataFormat::Unspecified)
+        {
+            std::cerr << "Cannot parse data format from file name, guessing uint8...\n";
+            dataFormat = vkt::DataFormat::UInt8;
+        }
+
+        vkt::StructuredVolume volume(dims.x, dims.y, dims.z, dataFormat);
+        vkt::InputStream is(file);
+        is.read(volume);
+
+        volumes[i - 1] = volume;
+    }
+
+    float rgba[] = {
+            1.f, 1.f, 1.f, .005f,
+            0.f, .1f, .1f, .25f,
+            .5f, .5f, .7f, .5f,
+            .7f, .7f, .07f, .75f,
+            1.f, .3f, .3f, 1.f
+            };
+    vkt::LookupTable lut(5,1,1,vkt::ColorFormat::RGBA32F);
+    lut.setData((uint8_t*)rgba);
+
+    vkt::RenderState renderState;
+    renderState.renderAlgo = vkt::RenderAlgo::RayMarching;
+    //renderState.renderAlgo = vkt::RenderAlgo::ImplicitIso;
+    //renderState.renderAlgo = vkt::RenderAlgo::MultiScattering;
+    renderState.rgbaLookupTable = lut.getResourceHandle();
+    vkt::RenderFrames(volumes, renderState);
+}
