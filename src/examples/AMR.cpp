e109b9d91262fb2f4bd99166fb588e422b0c6060
Stefan Zellmann <info@szellmann.de>
2021-08-05 15:13:25 +0200

    Some quick presets for the SILCC volume

--- src/examples/AMR.cpp
+++ src/examples/AMR.cpp
@@ -104,8 +104,8 @@ private:
 
 int main()
 {
-    Example dataSource;
-    // vkt::FLASHFile dataSource("/Users/stefan/volkit/build/SILCC_hdf5_plt_cnt_0300", "dens");
+    // Example dataSource;
+    vkt::FLASHFile dataSource("/Users/stefan/volkit/build/SILCC_hdf5_plt_cnt_0300", "dens");
 
     vkt::InputStream is(dataSource);
 
@@ -114,15 +114,15 @@ int main()
                                vkt::DataFormat::Float32);
     is.read(hv);
 
-    vkt::HierarchicalVolume hv2(nullptr, 0, vkt::DataFormat::Float32);
+    // vkt::HierarchicalVolume hv2(nullptr, 0, vkt::DataFormat::Float32);
 
-    vkt::CropResize(hv2, hv, {0,0,2}, {8,8,8});
-    vkt::Crop(hv2, hv, {0,0,2}, {8,8,8});
+    // vkt::CropResize(hv2, hv, {0,0,2}, {8,8,8});
+    // vkt::Crop(hv2, hv, {0,0,2}, {8,8,8});
 
-    // vkt::StructuredVolume sv(128,128,2560,vkt::DataFormat::UInt16);
-    vkt::StructuredVolume sv(32,32,24,vkt::DataFormat::UInt8);
+    vkt::StructuredVolume sv(128,128,2560,vkt::DataFormat::UInt16);
+    // vkt::StructuredVolume sv(32,32,24,vkt::DataFormat::UInt8);
 
-    vkt::Resample(sv, hv2, vkt::Filter::Linear);
+    vkt::Resample(sv, hv, vkt::Filter::Linear);
 
     float rgba[] = {
             1.f, 1.f, 1.f, .005f,
@@ -140,9 +140,23 @@ int main()
     vkt::SetThreadExecutionPolicy(ep);
 
     vkt::RenderState renderState;
+
+    // for SILCC volume
+    renderState.viewportWidth = 1555;
+    renderState.viewportHeight = 520;
+    renderState.initialCamera = { 1,{-206.179,159.237,1264.72},{54.7557,65.3722,1225.13},{0.33416,0.942009,-0.0310351},45.f,.001f,10.f };
+
     //renderState.renderAlgo = vkt::RenderAlgo::RayMarching;
     //renderState.renderAlgo = vkt::RenderAlgo::ImplicitIso;
     renderState.renderAlgo = vkt::RenderAlgo::MultiScattering;
     renderState.rgbaLookupTable = lut.getResourceHandle();
-    vkt::Render(sv, renderState);
+    vkt::RenderState out;
+    vkt::Render(sv, renderState, &out);
+    std::cout << "{" << out.initialCamera.eye.x << ',' <<  out.initialCamera.eye.y << ',' <<  out.initialCamera.eye.z << "}\n";
+    std::cout << "{" << out.initialCamera.center.x << ',' <<  out.initialCamera.center.y << ',' <<  out.initialCamera.center.z << "}\n";
+    std::cout << "{" << out.initialCamera.up.x << ',' <<  out.initialCamera.up.y << ',' <<  out.initialCamera.up.z << "}\n";
+    std::cout << out.initialCamera.fovy << '\n';
+    std::cout << out.initialCamera.lensRadius << '\n';
+    std::cout << out.initialCamera.focalDistance << '\n';
+    std::cout << out.viewportWidth << ' ' << out.viewportHeight << '\n';
 }
