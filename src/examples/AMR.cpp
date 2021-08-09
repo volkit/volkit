349decb18ff31a8c314722809ccd26b7cdaee946
Stefan Zellmann <info@szellmann.de>
2021-08-09 12:04:03 +0200

    Cleanup AMR example a little bit

--- src/examples/AMR.cpp
+++ src/examples/AMR.cpp
@@ -146,10 +146,6 @@ void read(vkt::HierarchicalVolume&  hv, std::string fileName) {
 
 int main()
 {
-    vkt::ExecutionPolicy ep = vkt::GetThreadExecutionPolicy();
-    ep.printPerformance = vkt::True;
-    vkt::SetThreadExecutionPolicy(ep);
-
     Example dataSource;
     // vkt::FLASHFile dataSource("/Users/stefan/volkit/build/SILCC_hdf5_plt_cnt_0300", "dens");
 
@@ -160,26 +156,16 @@ int main()
                                vkt::DataFormat::Float32);
     is.read(hv);
 
-    // vkt::HierarchicalVolume hv;
-    // read(hv,"dump.vkt");
-    // write(hv);
-
+    // Demonstrates Crop()    
     // vkt::HierarchicalVolume hv2(nullptr, 0, vkt::DataFormat::Float32);
-
     // vkt::CropResize(hv2, hv, {0,0,20480}, {4096,4096,61440});
     // vkt::Crop(hv2, hv, {0,0,20480}, {4096,4096,61440});
 
+    // Demonstrates resampling to structured volume
     // vkt::StructuredVolume sv(128,128,1280,vkt::DataFormat::UInt8);
     //vkt::StructuredVolume sv(32,32,640,vkt::DataFormat::UInt8);
     //vkt::StructuredVolume sv(1,1,20,vkt::DataFormat::UInt16,1.f,1.f,1.f,-28.1f,-21.f);
-
     // vkt::Resample(sv, hv2, vkt::Filter::Linear);
-
-    // vkt::StructuredVolume sv2(128,128,1280,vkt::DataFormat::UInt16);
-    // vkt::Resample(sv2, sv, vkt::Filter::Linear);
-
-    // ep.device = vkt::ExecutionPolicy::Device::CPU;
-    // vkt::SetThreadExecutionPolicy(ep);
 
     float rgba[] = {
             1.f, 1.f, 1.f, .005f,
@@ -192,31 +178,14 @@ int main()
     lut.setData((uint8_t*)rgba);
 
     // Switch execution to GPU (remove those lines for CPU rendering)
-    // vkt::ExecutionPolicy ep = vkt::GetThreadExecutionPolicy();
-    // ep.device = vkt::ExecutionPolicy::Device::GPU;
-    // vkt::SetThreadExecutionPolicy(ep);
-
+    vkt::ExecutionPolicy ep = vkt::GetThreadExecutionPolicy();
     ep.device = vkt::ExecutionPolicy::Device::GPU;
     vkt::SetThreadExecutionPolicy(ep);
 
     vkt::RenderState renderState;
-
-    // for SILCC volume
-    // renderState.viewportWidth = 1555;
-    // renderState.viewportHeight = 520;
-    // renderState.initialCamera = { 1, {-167.111,138.028,1276.09},{54.7994,58.2017,1242.42},{0.33416,0.942009,-0.0310351},45.f,.001f,10.f };
-
     //renderState.renderAlgo = vkt::RenderAlgo::RayMarching;
     //renderState.renderAlgo = vkt::RenderAlgo::ImplicitIso;
     renderState.renderAlgo = vkt::RenderAlgo::MultiScattering;
     renderState.rgbaLookupTable = lut.getResourceHandle();
-    vkt::RenderState out;
-    vkt::Render(hv, renderState, &out);
-    std::cout << "{" << out.initialCamera.eye.x << ',' <<  out.initialCamera.eye.y << ',' <<  out.initialCamera.eye.z << "}\n";
-    std::cout << "{" << out.initialCamera.center.x << ',' <<  out.initialCamera.center.y << ',' <<  out.initialCamera.center.z << "}\n";
-    std::cout << "{" << out.initialCamera.up.x << ',' <<  out.initialCamera.up.y << ',' <<  out.initialCamera.up.z << "}\n";
-    std::cout << out.initialCamera.fovy << '\n';
-    std::cout << out.initialCamera.lensRadius << '\n';
-    std::cout << out.initialCamera.focalDistance << '\n';
-    std::cout << out.viewportWidth << ' ' << out.viewportHeight << '\n';
+    vkt::Render(hv, renderState);
 }
